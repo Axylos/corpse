bdb54036785f6498938783b4289ef8a5400eb5ec
Axylos <robertdraketalley@gmail.com>
2017-11-27 14:45:17 -0500

    wip

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const app = require('./app');
 
-app.listen(3000, () => {
-  console.log('Corpse app listening on 3000');
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Corpse app listening on ${PORT}`);
 });

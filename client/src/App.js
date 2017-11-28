30c15553dbef077b6638731c42b7330355f3fdd2
Axylos <robertdraketalley@gmail.com>
2017-11-28 15:22:56 -0500

    display links in bottom nav

--- client/src/App.js
+++ client/src/App.js
@@ -5,24 +5,6 @@ import './App.css';
 import CorpseNav from './components/corpse_nav';
 
 
-/*
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
-*/
-
 const App = (props, context) =>
   (
     <MuiThemeProvider>
--- client/src/components/corpse_nav.js
+++ client/src/components/corpse_nav.js
@@ -2,12 +2,26 @@ import React, { Component } from 'react';
 import { BottomNavigation, BottomNavigationItem } from 'material-ui/BottomNavigation';
 import FontIcon from 'material-ui/FontIcon';
 
-const hiIcon = <FontIcon className="material-icons">favorite</FontIcon>;
+const homeIcon = <FontIcon className="material-icons">home</FontIcon>;
+const strangerIcon = <FontIcon className="material-icons">live_help</FontIcon>;
+const friendIcon = <FontIcon className="material-icons">group</FontIcon>;
+const resourcesIcon = <FontIcon className="material-icons">link</FontIcon>;
+const peruseIcon = <FontIcon className="material-icons">search</FontIcon>;
+const bestIcon = <FontIcon className="material-icons">star</FontIcon>;
+const contactIcon = <FontIcon className="material-icons">email</FontIcon>;
+const aboutIcon = <FontIcon className="material-icons">info</FontIcon>;
 
 const CorpseNav = props =>
   (
     <BottomNavigation>
-      <BottomNavigationItem label="Hi there" icon={hiIcon} />
+      <BottomNavigationItem label="Home" icon={homeIcon} />
+      <BottomNavigationItem label="Write With Strangers" icon={strangerIcon} />
+      <BottomNavigationItem label="Write With Friends" icon={friendIcon} />
+      <BottomNavigationItem label="Resources" icon={resourcesIcon} />
+      <BottomNavigationItem label="Peruse The Poems" icon={peruseIcon} />
+      <BottomNavigationItem label="Best Of" icon={bestIcon} />
+      <BottomNavigationItem label="Contact" icon={contactIcon} />
+      <BottomNavigationItem label="About" icon={aboutIcon} />
     </BottomNavigation>
   );
 
